e726139d9c4768f9519aeea113ba4658e75a4ec5
Sameer-Agrawal <sameerag0812@outlook.com>
2022-04-03 03:15:02 +0530

    infinite scroll

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,14 +6,14 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card">
-            <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex:'1'}}>
+            <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex:'1'}}>
               {source}
             </span>
           <img className="card-img-top" src={!imageUrl ? "https://i.hurimg.com/i/hdn/75/200x200/62441a674e3fe02e4427a540.jpg" : imageUrl} alt="Card image cap" />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <p class="card-text"><small class="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toUTCString()}</small></p>
+            <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toUTCString()}</small></p>
             <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
           </div>
         </div>
